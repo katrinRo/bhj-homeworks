abebbfb1687889d07393e661998d240f94372afc
E.Rodionova <makantero>
2023-03-11 18:49:00 +0300

    update task

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,14 +4,11 @@ let clickLast = new Date();
 let speedClick = document.getElementById("clicker__speed");
 function change(){
     let clickNow = new Date();
-    speedClick.textContent = ((clickNow - clickLast) / 1000).toFixed(2);
+    let time = clickNow - clickLast;
+    speedClick.textContent = (1 / (time/ 1000)).toFixed(2);
     clickLast = clickNow;
     countClick.textContent++;
-    if (countClick.textContent % 2 === 0) {
-        image.width = 200;
-    } else {
-        image.width = 220;
-    }
+    image.width = +countClick.textContent % 2 ?220: 200;
 }
 image.onclick = change;
 
