b48e340ba94946f9c3dfe279bba334e4be74e408
katrinRo <32965979+katrinRo@users.noreply.github.com>
2023-03-17 18:08:29 +0300

    update task

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,12 +4,15 @@ for (const i of hasTooltip) {
     let help = document.createElement('div');
     help.textContent = i.getAttribute('title');
     help.className = 'tooltip';
-    i.insertAdjacentElement('beforeBegin', help)
+    const {left, top} = i.getBoundingClientRect();
+    help.style.marginTop = 5 + 'px';
+    help.style.left = left + 'px';
+    i.insertAdjacentElement('afterend', help)
     i.addEventListener('click', (e) => {
         e.preventDefault();
         const isHelp = document.querySelector('.tooltip_active');
         if (isHelp) {
-            isHelp.remove();
+            isHelp.classList.remove('tooltip_active');
         }
         help.classList.add('tooltip_active');
     })
