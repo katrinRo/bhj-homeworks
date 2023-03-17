98d584ab507de5831776a93917fce1fda6531180
katrinRo <32965979+katrinRo@users.noreply.github.com>
2023-03-17 18:09:00 +0300

    update task

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,30 +2,28 @@ const addBtn = document.getElementById('tasks__add');
 const list = document.getElementById('tasks__list');
 
 function addTask(text) {
-    let task = document.createElement('div');
-    task.className = "task"
-    task.innerHTML += `
+    list.insertAdjacentHTML('afterend',`<div class = "task">
         <div class = "task__title">${text}
         </div>
         <a href="#" class ="task__remove">&times;</a>
-    `;
-    list.appendChild(task);
+	</div>
+    `);
 }
 
 function removeTask(e) {
     e.preventDefault();
     let parent = e.currentTarget.parentElement;
-    parent.remove()
+    parent.remove();
 }
 
 addBtn.addEventListener('click', (e) => {
     const inputText = document.getElementById('task__input');
     e.preventDefault();
-    if (inputText.value != "") {
+    const text = inputText.value.trim();
+    if (text != "") {
         addTask(inputText.value);
         inputText.value = "";
         const removeBtn = document.querySelectorAll('.task__remove');
-        console.log(removeBtn)
         for (const i of removeBtn) {
             i.addEventListener('click', removeTask)
         }
